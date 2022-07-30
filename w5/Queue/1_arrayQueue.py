896a840ee2b683d4a8d01aed7568a2fa8d6780fa
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2022-07-31 02:19:02 +0600

    queue intro & implementation done

---
+++ w5/Queue/1_arrayQueue.py
@@ -0,0 +1,47 @@
+
+class ArrayQueue:
+
+    def __init__(self,arr,st,sz):
+
+        self.size = sz
+        self.start = st
+        self.capacity = len(arr)
+        self.array = arr
+
+
+
+    def enqueue(self, item):
+        lastIdx = (self.start+self.size-1)%self.capacity
+        nextAvailablePos = (self.start+self.size)%self.capacity
+
+        self.array[nextAvailablePos] = item
+        self.size +=1
+
+    def dequeue(self):
+        
+        removed = self.array[self.start]
+        self.array[self.start] = None
+        self.start +=1
+        return removed
+
+    def peek(self):
+        return self.array[self.start]
+
+
+# ======================
+
+
+ar1 = [None, None, None, None, None, None]
+
+q1 = ArrayQueue(ar1,0,0)
+
+q1.enqueue(29)
+q1.enqueue(10)
+q1.enqueue(34)
+q1.enqueue(45)
+q1.enqueue(34)
+print(q1.array)
+
+print(q1.peek())
+print(q1.dequeue())
+print(q1.peek())
---
+++ w5/Queue/readme.md
@@ -0,0 +1,44 @@
+# Queue 
+*FIFO (First in First out Data structure)*
+
+Date: 31st July, 2022  
+
+## Introduction
+
+![Intro to Queue](https://static.studytonight.com/data-structures/images/introduction-to-queue.png)  
+
+![Intro to Queue](https://miro.medium.com/max/1400/0*TRbfsq86lqDoqW6b.png)  
+
+
+### Enqueue
+**Inserting in Queue:** 
+
+
+### Dequeue
+**Removing from Queue**  
+
+:point_right: Dequeue method remove & return item form the Queue
+
+
+### Peek  
+Peeking in a queue  
+
+:point_right: Peek method return item from the front of the queue
+
+
+
+### Applications of Queue: 
+
+Order of items in queue is based on time of arrival. Queue is used for first come first service.
+
+:point_right: Call centre phone system  
+:point_right: Waiting room management system  
+:point_right: CPU task scheduling  
+
+___
+
+<br>
+
+
+## Implementation of Queue 
+
